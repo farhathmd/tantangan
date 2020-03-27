2d6da250088e3678ca3b2de2b8e01f17616eceb8
farhathmd <farhat-2018@fst.unair.ac.id>
2020-03-27 16:24:58 +0700

    database

--- config/database.php
+++ config/database.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'default' => pgsql_production,
+    'default' => pgsql,
 
     /*
     |--------------------------------------------------------------------------
@@ -70,21 +70,6 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'pgsql_production' => array(
             'driver'   => 'pgsql',
             'host'     => $host,
             'database' => $database,
@@ -93,7 +78,7 @@ return [
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
-        ),
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
