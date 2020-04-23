be837cc27694550050cbdc87ce58f660870a4560
farhathmd <farhat-2018@fst.unair.ac.id>
2020-04-23 21:15:46 +0700

    register

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::get('/appointment', 'Appointment@input');
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
